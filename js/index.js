8ab001b77bb99f14a8eec6622ecb7bbd19db182d
EmmyKay0026 <kadiriemmanueloshio0026@gmail.com>
2024-03-07 14:11:14 +0100

    07/03/2024

---
+++ js/index.js
@@ -0,0 +1,27 @@
+// Variables
+// Declartion
+let name;
+// Assignment
+name = "DYEN"; //String
+
+let program = "Web design";
+
+// "" ''
+console.log(name + " has a " + program + " program");
+
+let age = "16";
+let a = 13;
+
+let dyenAge = 3;
+
+// console.log(age + (friendAge + dyenAge));
+
+let userName = prompt();
+
+console.log("Hello " + userName, " thank you for visiting");
+
+alert("Hello " + userName + ", thank you for visiting");
+
+userName = "Frank";
+
+console.log(userName);
