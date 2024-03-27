13b4b689cd911716311fecee50707530a0b5055c
EmmyKay0026 <kadiriemmanueloshio0026@gmail.com>
2024-03-27 15:59:34 +0100

    27/03

--- js/index.js
+++ js/index.js
@@ -1,112 +1,24 @@
-// // Variables
-// // Declartion
-// let name;
-// // Assignment
-// name = "DYEN"; //String
+const root = document.getElementById("root");
+const root2 = document.getElementById("root2");
 
-// let program = "Web design";
+// const fname = prompt("What is your first name");
+// const lname = prompt("What is your last name");
 
-// // "" ''
-// console.log(name + " has a " + program + " program");
-
-// let age = "16";
-// let a = 13;
-
-// let dyenAge = 3;
-
-// // console.log(age + (friendAge + dyenAge));
-
-// const userName = prompt();
-
-// console.log("Hello " + userName, " thank you for visiting");
-
-// alert("Hello " + userName + ", thank you for visiting");
-
-// userName = "Frank";
-
-// console.log(userName);
-
-// Data Types
-// Primitive data type
-//String
-//Numbers
-//Undefined
-//Null
-//Boolean
-
-// Reference data type
-// Object
-// Array
-
-// let dyen; // undefined
-
-// dyen = 564748947476;
-
-// const aliu = {
-//   fname: "Aliu",
-//   lname: "Ambrose",
-//   age: 16,
-//   address: {
-//     country: "Nigeria",
-//     state: "Delta",
-//     city: "Warri",
-//   },
-//   gender: "male",
-// };
-
-// Array
-//            0  1  2  3  4
-// const cars = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-// const studentNames = ["James", "Peter", "John", "Judas", "12", true];
-
-// console.log(typeof studentNames[4]);
-
-// if statement
-// if else
-// switch
-// if else if
-
-const dbPassword = "1234567890";
-// const inputtedPassword = prompt("What is your password", "Enter your password");
-
-// prompt("What is today?");
-
-// True or False
-// console.log(typeof dayOfTheWeek);
-// ==
-// ===
-
-// if ("a" == "A") {
-//   console.log(true);
-// } else console.log(false);
-
-// if (1 == "1") {
-//   alert("Today is Monday");
-// } else if (dayOfTheWeek == 2) {
-//   alert("Today is Tuesday");
-// } else if (dayOfTheWeek == "4") {
-//   alert("Today is Thursday");
-// } else if (dayOfTheWeek == 5) {
-//   alert("Today is Friday");
-// } else if (dayOfTheWeek == 6) {
-//   alert("Today is Saturday");
-// }
-
-// Switch Statement
-const age = 89;
-
-switch (age) {
-  case 1:
-    console.log("Infant");
-    break;
-  case 10:
-    console.log("Child");
-    break;
-  case 18:
-    console.log("Teens");
-    break;
-
-  default:
-    console.log("Adults");
-    break;
+function greet(name1, name2) {
+  root.innerText = `Hello Web development students, I am ${name1}  ${name2}`;
+  // root2.innerText = `Hello Web development students, I am `;
 }
+
+// greet("Faith", "Frank");
+// greet("Mary", "Ola");
+
+// greet(fname, lname);
+// greet();
+
+function add(num1, num2) {
+  const answer = num1 + num2;
+
+  return answer;
+}
+
+add(2, 4);
