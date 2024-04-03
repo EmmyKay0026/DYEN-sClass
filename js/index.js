67229ba5e3cb89c172e1e90a89416233332b34e2
EmmyKay0026 <kadiriemmanueloshio0026@gmail.com>
2024-04-03 16:02:02 +0100

    04/03/2024

--- js/index.js
+++ js/index.js
@@ -1,24 +1,30 @@
-const root = document.getElementById("root");
-const root2 = document.getElementById("root2");
+const bulbImg = document.getElementById("bulb-img");
+const turnOn = document.getElementById("turn-on");
+const turnOff = document.getElementById("turn-off");
+const bulbStatus = document.getElementById("bulb-status");
 
-// const fname = prompt("What is your first name");
-// const lname = prompt("What is your last name");
+turnOn.addEventListener("click", () => {
+  bulbImg.src = "../assets/onbulb.jpg";
+  bulbStatus.innerHTML = `
+  <span>To turn off the bulb, click the "Turn off button"</span>
+  `;
+  // bulbStatus.innerText = "The bulb is on.";
 
-function greet(name1, name2) {
-  root.innerText = `Hello Web development students, I am ${name1}  ${name2}`;
-  // root2.innerText = `Hello Web development students, I am `;
-}
+  // console.log(bulbImg.src);
+  console.log("the bulb is on");
+});
 
-// greet("Faith", "Frank");
-// greet("Mary", "Ola");
+turnOff.addEventListener("click", () => {
+  bulbImg.src = "../assets/goodbulb.jpg";
+  bulbStatus.innerHTML = "The bulb is off.";
 
-// greet(fname, lname);
-// greet();
+  console.log("the bulb is off");
+});
 
-function add(num1, num2) {
-  const answer = num1 + num2;
+// const applyBtn = document.getElementById("apply-btn");
 
-  return answer;
-}
+// // console.log(applyBtn);
 
-add(2, 4);
+// applyBtn.addEventListener("click", () => {
+//   console.log("Apply button was clicked");
+// });
